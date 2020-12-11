bdc12b3f34980bcca14f7a6762a9e7d4cca37848
bsiima <nuwabrenda@gmail.com>
2020-12-11 10:40:32 +0300

    changes made

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-//Route::get('/{name}' , 'firstcontroller@getWelcomepage');
+Route::get('/{name}' , 'firstcontroller@getWelcomepage');
 
-//Route::get('/{girl}', 'names@getPersons' );
-// Route::get('/{school}', 'school@getSchoolName' );
-// Route::get('/{pet}', 'animals@getPetName' );
-// Route::get('/{novel}', 'books@getNovel' );
+Route::get('/{girl}', 'names@getPersons' );
+Route::get('/{school}', 'school@getSchoolName' );
+Route::get('/{pet}', 'animals@getPetName' );
+Route::get('/{novel}', 'books@getNovel' );
  Route::get('/{city}', 'city@getTown');
